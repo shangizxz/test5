b05e9437c7a3c169b93fab84edf8b12b409f2158
Tricia Mae E. Bangalao <145832353+shangizxz@users.noreply.github.com>
2024-11-06 23:42:12 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ubuntu Server PHP Deployment</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body>
+
+	<p>Ubuntu Server-PHP Deployment<p>
+
+
+</body>
+
+<form action="your-server-endpoint" method="post">
+
+<div>
+	<label>Firstname</label>
+	<input type="" name="firstname"class="form-control">
+</div>
+
+<div>
+	<label>Middle Name</label>
+	<input type="" name="middlename"class="form-control">
+</div>
+
+
+<div>
+	<label>Lastname</label>
+	<input type="" name="lastname"class="form-control">
+</div>
+
+
+<div>
+	<label>Age</label>
+	<input type="" name="age"class="form-control">
+</div>
+
+<div>
+	<label>Address</label>
+	<input type="" name="address"class="form-control">
+</div>
+
+<div>
+	<label>Course & Section</label>
+	<input type="" name="courseandsection"class="form-control">
+</div>
+
+
+<div class="mt-3">
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</div>
+	</form>
+
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
